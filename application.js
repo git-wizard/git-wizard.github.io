82ca503eac6b8dff92965322e15a26c62dc40bb6
git-wizard <fawdz15@gmail.com>
2016-05-02 19:37:42 +0100

    image fade speed

--- application.js
+++ application.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   $(window).load(function() {          
   var i =0;
-  var images = ['firewatch.png','image2.png','background.jpg'];
+  var images = ['firewatch.png','image2.jpg','background.jpg'];
   var image = $('#background-image');
                 //Initial Background image setup
   image.css('background-image', 'url(background.jpg)');
                 //Change image at regular intervals
   setInterval(function(){  
-   image.fadeOut(1000, function () {
+   image.fadeOut(500, function () {
    image.css('background-image', 'url(' + images [i++] +')');
-   image.fadeIn(1000);
+   image.fadeIn(500);
    });
    if(i == images.length)
     i = 0;
-  }, 5000);           
+  }, 10000);           
  });
 });
