c7e39ac0f6ca868724b1f77ad26eaafd9131f531
git-wizard <alex.wzor@gmail.com>
2016-10-30 13:03:36 +0000

    New Website design

---
+++ js/app.js
@@ -0,0 +1,42 @@
+particlesJS('particles-js',
+
+{
+  "particles": {
+    "number": {
+      "value": 60,
+      "density.value_area": 3000
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 120,
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 4,
+      "direction": "none",
+      "straight": false
+    }
+  },
+  "interactivity": {
+        "modes": {
+          "grab": {
+            "distance": 200,
+        "line_linked": {
+          "opacity": 0.4
+        }
+      },
+     }
+  },
+  "retina_detect": true
+}
+
+);
